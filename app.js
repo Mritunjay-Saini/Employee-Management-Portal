78a5b66699af366ed5059116e3a17ce283247d00
Mritunjay Saini <140881890+Mritunjay-Saini@users.noreply.github.com>
2025-06-09 10:46:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let employees = [];
+let employees = JSON.parse(localStorage.getItem("employees")) || [];
 let editIndex = null;
 
 const form = document.getElementById("Employee-form");
@@ -6,6 +6,44 @@ const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const roleInput = document.getElementById("role");
 const tableBody = document.querySelector("#Employee-table tbody");
+
+
+function saveToLocalStorage() {
+  localStorage.setItem("employees", JSON.stringify(employees));
+}
+
+function renderTable() {
+  tableBody.innerHTML = "";
+  employees.forEach((emp, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${emp.name}</td>
+      <td>${emp.email}</td>
+      <td>${emp.role}</td>
+      <td class="action-btns">
+        <button onclick="editEmployee(${index})">Edit</button>
+        <button onclick="deleteEmployee(${index})">Delete</button>
+      </td>
+    `;
+    tableBody.appendChild(row);
+  });
+}
+
+function editEmployee(index) {
+  const emp = employees[index];
+  nameInput.value = emp.name;
+  emailInput.value = emp.email;
+  roleInput.value = emp.role;
+  editIndex = index;
+  form.querySelector("button").innerHTML = "Update Employee";
+}
+
+function deleteEmployee(index) {
+  employees.splice(index, 1);
+  saveToLocalStorage();
+  renderTable();
+}
+
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -26,41 +64,12 @@ form.addEventListener("submit", function (e) {
     form.querySelector("button").innerHTML = "Add Employee";
   }
 
+  saveToLocalStorage();   
   form.reset();
-  renderTable();
+  location.reload();      
 });
 
-function renderTable() {
-  tableBody.innerHTML = "";
-
-  employees.forEach((emp, index) => {
-    const row = document.createElement("tr");
-
-    row.innerHTML = `
-      <td>${emp.name}</td>
-      <td>${emp.email}</td>
-      <td>${emp.role}</td>
-      <td class="action-btns">
-        <button onclick="editEmployee(${index})">Edit</button>
-        <button onclick="deleteEmployee(${index})">Delete</button>
-      </td>
-    `;
-
-    tableBody.appendChild(row);
-  });
-}
-
-function editEmployee(index) {
-  const emp = employees[index];
-  nameInput.value = emp.name;
-  emailInput.value = emp.email;
-  roleInput.value = emp.role;
-  editIndex = index;
-  form.querySelector("button").innerHTML = "Update Employee";
-}
-
-function deleteEmployee(index) {
-  employees.splice(index, 1);
+window.onload = function () {
   renderTable();
-}
-
+  getName();
+};
